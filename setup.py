3a3b77d8b95450f8d490c0f6a2ea27bc44a5504d
magniff <tinysnippets@gmail.com>
2016-03-04 16:39:06 -0500

    Update version to 0.1.3.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ classifiers = [
 setuptools.setup(
     name='watch',
     description='Attribute controlling microframework.',
-    version='0.1.2',
+    version='0.1.3',
     license='MIT license',
     platforms=['unix', 'linux', 'osx', 'win32'],
     author='magniff',
