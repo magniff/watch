e287849bfe16dba5e30dc0e77e9f6756149ca2eb
magniff <tinysnippets@gmail.com>
2016-03-08 11:44:12 -0500

    Working on interfaces.

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -1,7 +1,12 @@
 import pytest
 from watch import (
-    watch_this, ArrayOf, Pred, CombineFrom, ArgumentCheckError, ResultCheckError
+    implements, ArrayOf, Pred, CombineFrom, ArgumentCheckError,
+    ResultCheckError, WatchMe
 )
+from watch.function_validator import CallableWithInterface
+
+
+from watch.builtins import InstanceOf
 
 
 Integer = Pred(lambda item: isinstance(item, int))
@@ -11,12 +16,12 @@ PositiveInteger = CombineFrom(Integer, Positive)
 
 def test_returns():
 
-    @watch_this()
-    def valid() -> ArrayOf(Integer):
+    @implements({'return': ArrayOf(Integer)})
+    def valid():
         return [1, 2, 3]
 
-    @watch_this()
-    def invalid() -> ArrayOf(Integer):
+    @implements({'return': ArrayOf(Integer)})
+    def invalid():
         return "hello the children!"
 
     assert valid()
@@ -27,18 +32,62 @@ def test_returns():
 
 def test_arguments():
 
-    @watch_this()
-    def factorial(value: PositiveInteger, mode=None) -> PositiveInteger:
-        return 1 if value == 1 else value * factorial(value - 1)
+    interface = {'value': PositiveInteger(), 'return': PositiveInteger()}
 
-    assert factorial(10) == 3628800
+    @implements(interface)
+    def factorial(value, mode=None):
+        return 1 if value == 1 else value * factorial(value=value - 1)
+
+    assert factorial(value=10) == 3628800
     assert factorial(value=10, mode=1) == 3628800
 
     with pytest.raises(ArgumentCheckError):
-        factorial("hello")
+        factorial(value="hello")
 
     with pytest.raises(ArgumentCheckError):
-        factorial(0)
+        factorial(value=0)
 
     with pytest.raises(ArgumentCheckError):
-        factorial(-10)
+        factorial(value=-10)
+
+
+def test_callable_with_interface():
+    interface = {
+        'a': InstanceOf(int), 'b': InstanceOf(int), 'return': InstanceOf(int)
+    }
+
+    another_interface = {
+        'a': InstanceOf(int), 'b': InstanceOf(int), 'return': InstanceOf(str)
+    }
+
+    @implements(interface)
+    def function_that_fits(a, b):
+        return a + b
+
+    @implements(another_interface)
+    def function_that_doesnt_fit(a, b):
+        return a + b
+
+    class A(WatchMe):
+        foo = CallableWithInterface(interface)
+
+    a = A()
+    a.foo = function_that_fits
+
+    with pytest.raises(AttributeError):
+        a.foo = function_that_doesnt_fit
+
+
+def test_function_does_not_implement_interface():
+    interface = {
+        'a': InstanceOf(int), 'b': InstanceOf(int), 'return': InstanceOf(int)
+    }
+
+    @implements(interface)
+    def function(a, b, c):
+        return a + c
+
+    with pytest.raises(ValueError):
+        @implements(interface)
+        def function1(a, c):
+            return a + c
--- watch/function_validator.py
+++ watch/function_validator.py
@@ -1,8 +1,7 @@
 from inspect import signature
-from functools import wraps
 
 from .attr_controllers import WatchMe, PredicateController
-from .builtins import Callable, Whatever
+from .builtins import Callable, Whatever, MappingOf, InstanceOf, HasAttr
 
 
 class ArgumentCheckError(TypeError):
@@ -13,60 +12,89 @@ class ResultCheckError(TypeError):
     pass
 
 
-class EyeOn(WatchMe, PredicateController):
-    attr_failed = Callable
-    result_failed = Callable
-    function = Callable
+MapOfPredicates = MappingOf(
+    keys_type=InstanceOf(str), values_type=InstanceOf(PredicateController)
+)
 
-    def __call__(self, function):
-        # this set triggers function validation agains Callable validator
+
+class InterfacedFunction(WatchMe):
+    function = Callable
+    interface = MapOfPredicates
+
+    # customization related stuff
+    result_failed_handler = Callable
+    argument_failed_handler = Callable
+
+    def compare_params(self):
+        return all(
+            name in signature(self.function).parameters for name in
+            (item for item in self.interface if item != 'return')
+        )
+
+    def __init__(self, function, interface):
+        self.interface = interface
         self.function = function
 
+        if not self.compare_params():
+            raise ValueError(
+                "Function %s doesn`t implement interface %s." %
+                (self.function.__name__, dict(self.interface))
+            )
+        # you may wanna override following fail handlers
+        self.result_failed_handler = default_result_failed_handler
+        self.argument_failed_handler = default_argument_failed_handler
+
+    def __call__(self, **kwargs):
         func_signature = signature(self.function)
-        annotations = function.__annotations__
+        arguments = func_signature.bind(**kwargs).arguments
 
-        @wraps(self.function)
-        def decorator(*args, **kwargs):
-            arguments = func_signature.bind(*args, **kwargs).arguments
+        # following code validates arguments passed to the function
+        for name, value in arguments.items():
+            if not self.interface.get(name, Whatever).predicate(value):
+                self.argument_failed_handler(self.function, name, value)
 
-            # following code validates arguments passed to the function
-            for name, value in arguments.items():
-                checker = annotations.get(name)
+        # ok, lets evaluate function and validate the result
+        return_checker = self.interface.get('return', Whatever)
+        result = self.function(**kwargs)
+        if not return_checker.predicate(result):
+            self.result_failed_handler(self.function, kwargs, result)
 
-                if not checker:
-                    continue
-
-                if not checker.predicate(value):
-                    self.attr_failed(function, name, value)
-
-            # ok, lets evaluate function and validate the result
-            return_checker = annotations.get('return', Whatever)
-            result = function(*args, **kwargs)
-            if not return_checker.predicate(result):
-                self.result_failed(function, args, kwargs, result)
-
-            return result
-
-        return decorator
-
-    def __init__(self, attr_failed=None, result_failed=None):
-        self.attr_failed = attr_failed or default_attr_failed_handler
-        self.result_failed = result_failed or default_result_failed_handler
+        return result
 
 
-# little ailas to make function like name
-watch_this = EyeOn
+class CallableWithInterface(WatchMe, PredicateController):
+    interface = MapOfPredicates
+
+    def predicate(self, value):
+        precondition = (
+            InstanceOf(InterfacedFunction).predicate(value) and
+            HasAttr('interface').predicate(value) and
+            MapOfPredicates.predicate(value.interface)
+        )
+
+        return precondition and value.interface == self.interface
+
+    def __init__(self, inteface):
+        self.interface = inteface
 
 
-def default_result_failed_handler(func, args, kwargs, result):
+def default_result_failed_handler(func, kwargs, result):
     raise ResultCheckError(
         "Result %s of function %s failed validation." %
         (result, func.__name__)
     )
 
 
-def default_attr_failed_handler(func, attr_name, attr_value):
+def default_argument_failed_handler(func, attr_name, attr_value):
     raise ArgumentCheckError(
         "Argument %s == %s of function %s failed validation." %
         (attr_name, attr_value, func.__name__)
     )
+
+
+def implements(interface):
+    def builder(function):
+        interfaced_function = InterfacedFunction(function, interface)
+        return interfaced_function
+
+    return builder
